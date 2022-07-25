3e1073f4baf2f0541ac25e1d76a7398df4abeb6d
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-25 22:14:42 +0200

    Create controllers and its views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users , only: [:index, :show] do
+    resources :foods,:recipes  , only: [:index, :show, :new, :create, :destroy] do
+      resources :recipe_foods , only: [:index, :show, :new, :create, :destroy]
+    end
+  end 
+  get '/public_recipes', to: 'recipes#public_recipes'
 end
