8722008960fbb7a5f6be114f87d0708a7df22ea2
dewslyse <dewslyse@gmail.com>
2022-07-26 19:07:57 +0000

    Add actions to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
 class RecipesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @recipes = @user.recipes.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    # I am just going to put comment here
+  end
+
+  def show
+    @recipes = Recipe.find(params[:id])
+  end
+
+  def public_recipes
+    @recipes = Recipe.where(public: true)
+  end
+
+  def destroy
+    @post = Recipe.find(params[:id])
+    @post.destroy
+    flash[:notice] = 'Recipe removed successfully'
+    redirect_to '/recipes'
+  end
 end
