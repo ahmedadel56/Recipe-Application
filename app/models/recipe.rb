6c834cf2a3905dead700bb67b4db5d7d513658fe
dewslyse <dewslyse@gmail.com>
2022-07-26 19:07:40 +0000

    Add associations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_food
+
+  validates :name, presence: true
+  validates :preparation_time, :cooking_time, presence: true, numericality: { greater_than: 0, only_float: true }
+  validates :description, presence: true
+  validates :public, inclusion: { in: [true, false] }
 end
