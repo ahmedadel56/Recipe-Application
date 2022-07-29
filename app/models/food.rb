e4edf6c2caf5dfaad7bceaf6a2e2f81f0dee9410
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-29 19:50:38 +0200

    Fix routes problems

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   validates :name, :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than: 0, only_float: true }
 end
