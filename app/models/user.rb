8ac5c012bef9f4deabd74151f4b5791a7bbc47f1
dewslyse <dewslyse@gmail.com>
2022-07-29 12:12:11 +0000

    Add unit tests for model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes
   has_many :foods
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    user = User.create(name: 'Test User', email: 'tuser2@example.com', password: 'password')
+
+    @recipe = Recipe.create(user_id: user.id, name: 'Test Recipe', preparation_time: 1, cooking_time: 1,
+                            description: 'Test Description', public: true)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@recipe).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    @recipe.name = nil
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'is not valid without a preparation time' do
+    @recipe.preparation_time = nil
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'is not valid without a cooking time' do
+    @recipe.cooking_time = nil
+    expect(@recipe).to_not be_valid
+  end
 end
