f4ed41180782b590e653eea78750daa627d6a399
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-30 02:28:28 +0200

    Add Selase tests

--- spec/views/public_recipes_index_spec.rb
+++ spec/views/public_recipes_index_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'Public Recipes', type: :feature do
     @user = User.create(name: 'test', email: 'test@email.com', password: 'password')
     @recipe1 = Recipe.create(name: 'recipe1', user_id: @user.id, public: true, description: 'description')
     @recipe2 = Recipe.create(name: 'recipe2', user_id: @user.id, public: true, description: 'description2')
-    
-    sign_in @user    
+
+    sign_in @user
     visit public_recipes_path
   end
- 
+
   it 'renders the public recipe index page' do
     expect(page).to have_content('Public Recipes')
   end
